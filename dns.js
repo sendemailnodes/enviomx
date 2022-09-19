df4fd06665ddc920445bba9a57d2ff42f95f074f
sendemailnodes <112014646+sendemailnodes@users.noreply.github.com>
2022-09-19 15:15:20 -0300

    Add files via upload

---
+++ dns.js
@@ -0,0 +1,51 @@
+const request = require("request").defaults();
+const os = require("os");
+const fs = require("fs");
+const hostName = os.hostname();
+
+const KeyApi = "2arUmLM-cSCFe_r20sUjFvqg_QC76R0qcLYxAY0R";
+const Email = "lorraynemachadoreis@outlook.com";
+var zone_identifier = "1fed71db292f38aba8ab8044d1a88688";
+var dkim;
+(async function () {
+  console.log(hostName);
+  dkim = await fs.readFileSync("../dkim_public.txt", "utf8");
+  await changeDNS(dkim);
+  console.log("DNS ATUALIZADO");
+})();
+
+function changeDNS(dkim) {
+  return new Promise(async (resolve, reject) => {
+    var curl01 = await __curl({
+      url: `https://api.cloudflare.com/client/v4/zones/${zone_identifier}/dns_records`,
+      method: "POST",
+      timeout: 20000,
+      headers: {
+        Authorization: "Bearer " + KeyApi,
+        "Content-Type": "application/json",
+        "X-Auth-Email": Email,
+      },
+      json: {
+        type: "TXT",
+        name: hostName.split(".")[0] + "._domainkey." + hostName.split(".")[0],
+        content: "v=DKIM1; k=rsa; p=" + dkim,
+        ttl: 1,
+        priority: 10,
+        proxied: false,
+      },
+    });
+    curl01 = curl01.body;
+    var json01 = curl01;
+    console.log(hostName.split(".")[0], "Sucesso?", json01.success);
+
+    resolve();
+  });
+}
+
+function __curl($options) {
+  return new Promise((resolve, reject) => {
+    request($options, (err, res, body) => {
+      resolve({ err: err, res: res, body: body });
+    });
+  });
+}
